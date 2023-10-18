df074e891b95865dfa9cfca5658d3ff228a5e004
akandeifeoluwa55@gmail.com <akandeifeoluwa55@gmail.com>
2023-10-17 19:52:12 -0700

    Added my name and email address

--- mine.py
+++ mine.py
@@ -1,6 +1,6 @@
 # Define variables
-name = "PUT YOUR NAME"
-email = 'PUT YOUR EMAIL'
+name = "Taofik Akande"
+email = 'akandeifeloluwa55@gmail.com'
 
 # Print the name and email address
 print("Name:", name)
